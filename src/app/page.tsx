57704c13c36ae0456ccea82bcb5b1c2770512bf8
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-03 23:40:20 +0300

    feat: import data and create types file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-const Home = () => {
-  return <div>Home</div>;
+import { fetchApi } from "@/api";
+import CryptoTable from "@/components/CryptoTable";
+
+const Home = async () => {
+  const data = await fetchApi();
+
+  return (
+    <>
+      <h1>Crypto Tracker</h1>
+      <CryptoTable data={data} />
+    </>
+  );
 };
 
 export default Home;
