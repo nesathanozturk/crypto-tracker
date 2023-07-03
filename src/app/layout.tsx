0097b4b1a5b3c5aadb9509a1583c61486fb8d8f5
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 00:59:57 +0300

    feat: added inputprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { InputProvider } from "../context/InputContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <InputProvider>
+        <body className={inter.className}>{children}</body>
+      </InputProvider>
     </html>
   );
 }
--- src/context/InputContext.tsx
+++ src/context/InputContext.tsx
@@ -14,3 +14,5 @@ function InputProvider({ children }: IChildren) {
     </InputContext.Provider>
   );
 }
+
+export { InputProvider };
