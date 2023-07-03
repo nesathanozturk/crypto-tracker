517abd8357851913478fea1c1ede6b1283205d1b
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 01:03:44 +0300

    feat: import context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { useContext } from "react";
+import { InputContext } from "@/context/InputContext";
+import { IContext } from "@/types";
+
 const Header = () => {
+  const { term, setTerm } = useContext(InputContext) as IContext;
+
   return (
     <header className="flex justify-center items-center flex-col mb-14 mt-2">
       <h1 className="font-semibold text-2xl mb-4">Crypto Tracker</h1>
@@ -6,6 +14,8 @@ const Header = () => {
         type="text"
         placeholder="Search a coin..."
         className="input input-bordered input-primary w-full max-w-xs"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
       />
     </header>
   );
