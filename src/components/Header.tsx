2c4b50d15c564e53d4017aff158eeccff0b3bd87
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 00:35:40 +0300

    style: edited margib-top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="mb-14 mt-5 flex justify-center items-center flex-col">
+    <header className="flex justify-center items-center flex-col mb-14 mt-2">
       <h1 className="font-semibold text-2xl mb-4">Crypto Tracker</h1>
       <input
         type="text"
