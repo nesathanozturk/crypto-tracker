47b2a73f7b0d3bcee814917ff2270e17d126c7d9
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 01:13:59 +0300

    feat: added context

--- src/components/CryptoTable.tsx
+++ src/components/CryptoTable.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import { ICryptoData } from "@/types";
 import Crypto from "./Crypto";
+import { useContext } from "react";
+import { InputContext } from "@/context/InputContext";
+import { IContext } from "@/types";
 
 const CryptoTable: React.FC<ICryptoData> = ({ cryptoData }) => {
+  const { term } = useContext(InputContext) as IContext;
+
   return (
     <div className="overflow-x-auto">
       <table className="table">
@@ -16,9 +23,21 @@ const CryptoTable: React.FC<ICryptoData> = ({ cryptoData }) => {
           </tr>
         </thead>
         <tbody>
-          {cryptoData?.map((crypto) => (
-            <Crypto key={crypto?.id} crypto={crypto} />
-          ))}
+          {cryptoData
+            ?.filter((crypto) => {
+              if (term === "") {
+                return cryptoData;
+              } else if (
+                crypto.name
+                  .toLocaleLowerCase()
+                  .includes(term.toLocaleLowerCase())
+              ) {
+                return cryptoData;
+              }
+            })
+            .map((crypto) => (
+              <Crypto key={crypto?.id} crypto={crypto} />
+            ))}
         </tbody>
       </table>
     </div>
