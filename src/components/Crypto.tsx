4b171992481e375380e06ddc1809494a76a22c03
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 11:40:26 +0300

    chore: removed fragments

--- src/components/Crypto.tsx
+++ src/components/Crypto.tsx
@@ -6,26 +6,19 @@ const Crypto: React.FC<ICryptoProps> = ({ crypto }) => {
     crypto?.price_change_percentage_24h > 0 ? "text-green-500" : "text-red-500";
 
   return (
-    <>
-      <tr className="hover cursor-pointer">
-        <td className="flex items-center gap-3">
-          <Image
-            src={crypto?.image}
-            alt={crypto?.name}
-            width={30}
-            height={30}
-          />
-          {crypto?.name}
-        </td>
-        <td>{crypto?.symbol.toUpperCase()}</td>
-        <td>${crypto?.current_price}</td>
-        <td>${crypto?.total_volume}</td>
-        <td className={priceChangeColor}>
-          {crypto?.price_change_percentage_24h}
-        </td>
-        <td>${crypto?.market_cap}</td>
-      </tr>
-    </>
+    <tr className="hover cursor-pointer">
+      <td className="flex items-center gap-3">
+        <Image src={crypto?.image} alt={crypto?.name} width={30} height={30} />
+        {crypto?.name}
+      </td>
+      <td>{crypto?.symbol.toUpperCase()}</td>
+      <td>${crypto?.current_price}</td>
+      <td>${crypto?.total_volume}</td>
+      <td className={priceChangeColor}>
+        {crypto?.price_change_percentage_24h}
+      </td>
+      <td>${crypto?.market_cap}</td>
+    </tr>
   );
 };
 
