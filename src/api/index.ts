b98d4ada792c4ee9e2a55642c68f19d553edaf37
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 11:41:51 +0300

    feat: created const

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,10 @@
 import { ICrypto } from "@/types";
 
+const API_URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
 export const fetchApi = async (): Promise<ICrypto[]> => {
-  const res = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-  );
+  const res = await fetch(API_URL);
   const data = await res.json();
   return data;
 };
