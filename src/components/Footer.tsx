c0dbcc8584d2927e47f12fc2659922982299d117
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 11:39:32 +0300

    style: edited p

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="pt-5 pb-2 text-center">
-      <p>
+      <p className="font-semibold">
         Made with by{" "}
         <Link
           href="https://www.github.com/nesathanozturk"
-          className="text-purple-500 hover:underline"
+          className="text-purple-500 underline"
         >
           Neşathan Öztürk
         </Link>
