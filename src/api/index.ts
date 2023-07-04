975e6942385f3ed17c8643427a4bcb02298a4ac4
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 12:13:37 +0300

    feat: created env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,7 @@
 import { ICrypto } from "@/types";
 
-const API_URL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-
 export const fetchApi = async (): Promise<ICrypto[]> => {
-  const res = await fetch(API_URL);
+  const res = await fetch(process.env.API_URL as string);
   const data = await res.json();
   return data;
 };
