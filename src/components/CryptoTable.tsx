1496cddfefa285bf73dae4d024c21206aebd2c7a
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 11:32:00 +0300

    feat: added filtered fucntions

--- src/components/CryptoTable.tsx
+++ src/components/CryptoTable.tsx
@@ -9,38 +9,38 @@ import { IContext } from "@/types";
 const CryptoTable: React.FC<ICryptoData> = ({ cryptoData }) => {
   const { term } = useContext(InputContext) as IContext;
 
+  const isCoinFound = cryptoData?.every(
+    (crypto) => !crypto.name.toLowerCase().includes(term.toLowerCase())
+  );
+
+  const filteredCryptoData = cryptoData
+    ?.filter((crypto) => crypto.name.toLowerCase().includes(term.toLowerCase()))
+    ?.map((crypto) => <Crypto key={crypto.id} crypto={crypto} />);
+
   return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Symbol</th>
-            <th>Price</th>
-            <th>Total Volume</th>
-            <th>Change(24h)</th>
-            <th>Market Cap</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cryptoData
-            ?.filter((crypto) => {
-              if (term === "") {
-                return cryptoData;
-              } else if (
-                crypto.name
-                  .toLocaleLowerCase()
-                  .includes(term.toLocaleLowerCase())
-              ) {
-                return cryptoData;
-              }
-            })
-            .map((crypto) => (
-              <Crypto key={crypto?.id} crypto={crypto} />
-            ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      {isCoinFound ? (
+        <h1 className="py-10 text- md:text-2xl text-gray-400 text-center underline">
+          Coin you are looking for is not found!
+        </h1>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Coin</th>
+                <th>Symbol</th>
+                <th>Price</th>
+                <th>Total Volume</th>
+                <th>Change(24h)</th>
+                <th>Market Cap</th>
+              </tr>
+            </thead>
+            <tbody>{filteredCryptoData}</tbody>
+          </table>
+        </div>
+      )}
+    </>
   );
 };
 
