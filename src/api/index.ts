f221093386fcf44d95049b906c94a4b4e7c21651
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-04 12:19:52 +0300

    feat: no-cache

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import { ICrypto } from "@/types";
 
 export const fetchApi = async (): Promise<ICrypto[]> => {
-  const res = await fetch(process.env.API_URL as string);
+  const res = await fetch(process.env.API_URL as string, {
+    cache: "no-cache",
+  });
   const data = await res.json();
   return data;
 };
